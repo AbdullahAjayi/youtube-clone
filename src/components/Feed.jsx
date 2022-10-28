e93ed8724360a7dac82197a4355cc0a096870042
AbdullahAjayi <hafizajayi2308@gmail.com>
2022-10-28 18:25:29 +0100

    added nav, search and side bars

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <Stack
+      sx={{
+        flexDirection: { xs: "column", md: "row" },
+      }}
+    >
+      <Box
+        sx={{
+          height: { xs: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { xs: 0, md: 2 },
+        }}
+      >
+        SideBar
+        <Typography
+          className="copy-right"
+          variant="body2"
+          sx={{
+            mt: 1.5,
+            color: "#fff",
+          }}
+        >
+          Copyright 2022 Abdullah Ajayi
+        </Typography>
+      </Box>
+    </Stack>
+  );
 };
 
 export default Feed;
