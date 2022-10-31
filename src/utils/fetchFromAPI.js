9aee2d39b8b97e36c6dc008d399e6667eb0e6af8
AbdullahAjayi <hafizajayi2308@gmail.com>
2022-10-31 17:15:46 +0100

    added safe search filter

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -4,7 +4,8 @@ const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 
 const options = {
   params: {
-    maxResults: "50",
+    maxResults: "100",
+    safeSearch: "strict",
   },
   headers: {
     "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
